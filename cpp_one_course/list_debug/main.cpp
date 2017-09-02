bd0f23584459aff540062e1e89e0722355287ab0
Pavel <pavel-itg@rambler.ru>
2017-09-02 20:24:30 +0300

    /

--- cpp_one_course/list_debug/main.cpp
+++ cpp_one_course/list_debug/main.cpp
@@ -3,28 +3,39 @@
 
 using namespace std;
 
-template class list<int>;
+template class list_debug<int>;
 
 int main()
 {
     cout << "Hello World!" << endl;
-    int a[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, cnt = 10;
-    list<int> L;
-    for (int i = 0; i < cnt; i++) {
-        L.push_back(a[i]);
-    }
-    for (int i = 0; i < cnt; i++) {
-        cout << L.back() << " ";
-        L.pop_back();
-    }
-    cout << endl;
-    for (int i = 0; i < cnt; i++) {
-        L.push_front(a[i]);
-    }
-    for (int i = 0; i < cnt; i++) {
-        cout << L.front() << " ";
-        L.pop_front();
-    }
-    cout << endl;
+    list_debug<int> c;
+        for (int i = 0; i < 10; ++i) c.push_back(i);
+        for (int i = 1; i < 10; ++i) c.push_front(i);
+        for (int i = 10; i < 20; ++i) c.insert(c.begin(), i);
+        for (auto it = c.begin(); it != c.end(); ++it) {
+            cout << *it << " ";
+        }
+        list_debug<int> p;
+        for (int i = 0; i < 10; ++i) p.push_back(-1);
+        cout << endl;
+
+        auto it = c.begin();
+        for (int i = 0; i < 10; ++i) it++;
+
+        for (auto it = p.begin(); it != p.end(); ++it) {
+            cout << *it << " ";
+        }
+        cout << endl;
+        for (auto it = c.begin(); it != c.end(); ++it) {
+            cout << *it << " ";
+        }
+        cout << endl;
+        c.splice(it, p, p.begin(), p.end());
+
+        cout << endl;
+        for (auto it = c.begin(); it != c.end(); ++it) {
+            cout << *it << " ";
+        }
+        cout << endl;
     return 0;
 }
