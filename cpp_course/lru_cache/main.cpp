ee8f7c7be474bf68b3649f681a4574b0ba8a8144
GoPavel <pavel-itg@rambler.ru>
2017-09-18 23:29:14 +0300

    add main_lru_cache

---
+++ cpp_course/lru_cache/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "lru_cache.h"
+
+using namespace std;
+
+template class cache<int, int>;
+
+int main()
+{
+    cout << "Hi! I'm LRU Cache. I promise work without bug." << endl;
+    return 0;
+}
