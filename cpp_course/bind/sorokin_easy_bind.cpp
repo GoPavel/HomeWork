c57e9d5118b0184008e9ea54e7e5bc18ebdb84b5
Golovin Pavel <pavel-itg@rambler.ru>
2017-10-21 12:43:12 +0300

    modify for rec_bind

--- cpp_course/bind/sorokin_easy_bind.cpp
+++ cpp_course/bind/sorokin_easy_bind.cpp
@@ -30,6 +30,9 @@ template <int N>
 struct placeholder
 {};
 
+template <typename F, typename ... As>
+struct bind_t;
+
 constexpr placeholder<1> _1;
 constexpr placeholder<2> _2;
 constexpr placeholder<3> _3;
@@ -78,6 +81,22 @@ struct G<placeholder<N> >
 };
 
 template <typename F, typename ... As>
+struct G<bind_t<F, As...> >
+{
+    G(bind_t<F, As...> fun)
+        : fun(fun)
+    {}
+
+    template <typename ... Bs>
+    decltype(auto) operator()(Bs ... bs) const
+    {
+        return fun(bs...);
+    }
+
+    bind_t<F, As...> fun;
+};
+
+template <typename F, typename ... As>
 struct bind_t
 {
     bind_t(F f, As ... as)
@@ -115,10 +134,16 @@ void f(int a, int b, int c)
     std::cout << a << b << c << std::endl;
 }
 
+int g(int a, int b)
+{
+    return a + b;
+}
+
 int main()
 {
     bind(&f, 1, 2, 3)();
     bind(&f, _1, _3, _2)(4, 6, 5);
     bind(&f, _1, _1, _2)(7, 8);
+    bind(&f, _1, bind(&g, _1, 5), _2)(2, 3);
     return 0;
 }
